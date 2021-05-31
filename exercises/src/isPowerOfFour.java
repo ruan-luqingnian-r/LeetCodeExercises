e81abda2dee0324abbfbf862ee149aca8ca98c7e
ruan-luqingnian-r <15991875018@163.com>
2021-05-31 10:14:48 +0800

    LeetCode——342. 4的幂

---
+++ exercises/src/isPowerOfFour.java
@@ -0,0 +1,20 @@
+/**
+ * @PackgeName: PACKAGE_NAME
+ * @ClassName: isPowerOfFour
+ * @Author: 小天才
+ * Date: 2021/5/31 9:54
+ * project name: LeetCodeExercises
+ * @Version: 0.0.1
+ * @Description: LeetCode——342. 4的幂
+ */
+public class isPowerOfFour {
+    public static void main(String[] args) {
+        isPowerOfFour isPowerOfFour = new isPowerOfFour();
+        isPowerOfFour.isPowerOfFour(16);
+    }
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        int x = (int)Math.sqrt(n);
+        return x * x == n && (x & -x) == x;
+    }
+}
