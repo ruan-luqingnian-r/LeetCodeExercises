f025d860e9ec3498adb03dcb4c6d2f00d3a9074e
ruan-luqingnian-r <15991875018@163.com>
2021-07-16 16:37:06 +0800

    123

---
+++ simple/Send.java
@@ -0,0 +1,35 @@
+package simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author: ruan
+ * Date: 2021/7/16 15:53
+ * @Description: 发送队列
+ */
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.15.131");
+        factory.setUsername("admin");
+        factory.setPassword("password");
+        factory.setVirtualHost("/dev");
+        factory.setPort(5672);
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
