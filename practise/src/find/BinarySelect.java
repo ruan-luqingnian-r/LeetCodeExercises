3d8c9c05f0efea71e626269e560a0ea892bc38bb
ruan-luqingnian-r <15991875018@163.com>
2021-09-06 07:24:05 +0800

    BinarySelect

--- practise/src/find/BinarySelect.java
+++ practise/src/find/BinarySelect.java
@@ -1,9 +1,22 @@
 package find;
 
+import java.util.ArrayList;
+
+import static find.LinearSearch.select;
+
 /**
  * @Author: ruan
  * Date: 2021/9/5 19:52
  * @Description:
  */
 public class BinarySelect {
+    public static void main(String[] args) {
+        int[] arr = {1, 4,4,4, 12, 23, 31, 32, 42, 53, 62, 124, 124, 234, 235, 312, 312, 312, 412, 412, 3124, 3534, 4242, 23124};
+        System.out.println(select(arr,4,0,arr.length - 1));
+    }
+
+    private static ArrayList<String> select(int[] arr, int target, int left, int right) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        return arrayList;
+    }
 }
