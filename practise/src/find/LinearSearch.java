43272d0bd8543c2a7e4966a62b561f6eaa0b64c0
ruan-luqingnian-r <15991875018@163.com>
2021-09-05 17:34:22 +0800

    BubbleSort

--- practise/src/find/LinearSearch.java
+++ practise/src/find/LinearSearch.java
@@ -9,4 +9,11 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {1, 4, 12, 23, 31, 32, 42, 53, 62, 124, 124, 234, 235, 312, 312, 312, 412, 412, 3124, 3534, 4242, 23124};
     }
+
+    public static int select(int[] arr,int target){
+
+
+
+        return -1;
+    }
 }
