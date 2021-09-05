0268c455364609333169bc8e5c99a19070503aeb
ruan-luqingnian-r <15991875018@163.com>
2021-09-05 19:31:16 +0800

    BubbleSort

--- practise/src/find/LinearSearch.java
+++ practise/src/find/LinearSearch.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
  */
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {1, 4, 12, 23, 31, 32, 42, 53, 62, 124, 124, 234, 235, 312, 312, 312, 412, 412, 3124, 3534, 4242, 23124};
-        System.out.println(select(arr,1113));
+        int[] arr = {1, 4,4,4, 12, 23, 31, 32, 42, 53, 62, 124, 124, 234, 235, 312, 312, 312, 412, 412, 3124, 3534, 4242, 23124};
+        System.out.println(select(arr,4));
     }
 
     public static ArrayList<String> select(int[] arr, int target){
