d037603f71009231f6c95bafd7eabb00735bc6dd
ruan-luqingnian-r <15991875018@163.com>
2021-09-05 17:24:15 +0800

    BubbleSort

--- practise/src/sort/BubbleSort.java
+++ practise/src/sort/BubbleSort.java
@@ -7,6 +7,7 @@ package sort;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-
+        int[] arr = {23,32,42,1,312,412,31,4242,312,312,412,3124,3534,53,62,4,124,235,234,124,23124,12};
     }
+
 }
