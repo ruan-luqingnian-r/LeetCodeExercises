c69cb73dc0eb84f37942411056721da08eb5076e
ruan-luqingnian-r <15991875018@163.com>
2021-09-07 14:44:38 +0800

    test

--- practise/src/test/Test.java
+++ practise/src/test/Test.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         int[] arr = {23,32,42,1,312,412,31,4242,312,312,412,3124,3534,53,62,4,124,235,234,124,23124,12};
-        selectSort(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -61,7 +61,15 @@ public class Test {
      */
     public static void insertionSort(int[] arr){
         for (int i = 1; i < arr.length; i++) {
-
+            //待插入的数据
+            int value = arr[i];
+            //待插入数据的前一个下标
+            int index = i - 1;
+            while (index >= 0 && arr[index] > value){
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = value;
         }
     }
 
