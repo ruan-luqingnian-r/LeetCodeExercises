8d553f7145b05842a6d2dce548e91abe8cb0d3e5
ruan-luqingnian-r <15991875018@163.com>
2021-09-08 14:37:58 +0800

    生产者消费者模型 wait()/notify()方式实现

--- practise/src/demo/ProducerConsumerModel.java
+++ practise/src/demo/ProducerConsumerModel.java
@@ -3,7 +3,10 @@ package demo;
 /**
  * @Author: ruan
  * Date: 2021/9/8 14:32
- * @Description: 生产者消费者模型
+ * @Description: 生产者消费者模型 wait()/notify()方式实现
  */
 public class ProducerConsumerModel {
+    public static void main(String[] args) {
+
+    }
 }
