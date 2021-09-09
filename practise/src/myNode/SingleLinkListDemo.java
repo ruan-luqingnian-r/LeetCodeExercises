152a15b78b859ac3aaa28bae9d0169f6e0f30728
ruan-luqingnian-r <15991875018@163.com>
2021-09-09 12:07:09 +0800

    单链表

--- practise/src/myNode/SingleLinkListDemo.java
+++ practise/src/myNode/SingleLinkListDemo.java
@@ -5,17 +5,38 @@ package myNode;
  * Date: 2021/9/9 7:57
  * @Description:
  */
-public class SingleLinkList {
+public class SingleLinkListDemo {
 
 }
 
 /**
- * 节点
+ * 自定义节点
  */
 class Node{
+    /**
+     * 节点编号
+     */
     private int no;
+    /**
+     * 节点名称（内容）
+     */
     private String name;
+    /**
+     * 指向下一个节点
+     */
     private Node next;
+
+    /**
+     * 创建构造方法
+     * @param no
+     * @param name
+     * @param next
+     */
+    public Node(int no, String name, Node next) {
+        this.no = no;
+        this.name = name;
+        this.next = next;
+    }
 
     public int getNo() {
         return no;
@@ -40,4 +61,11 @@ class Node{
     public void setNext(Node next) {
         this.next = next;
     }
+}
+
+/**
+ * 自定义链表
+ */
+class SinglyLinkedList{
+
 }
