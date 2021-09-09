15c42bd69b39766e23f0aab5431ae09d3c5eba83
ruan-luqingnian-r <15991875018@163.com>
2021-09-09 11:56:43 +0800

    链表节点

---
+++ practise/src/myNode/Node.java
@@ -0,0 +1,33 @@
+package myNode;
+
+/**
+ * @Author: ruan
+ * Date: 2021/9/9 7:57
+ * @Description:
+ */
+public class Node {
+    /**
+     * 链表的id
+     */
+    private Integer id;
+    /**
+     * 链表
+     */
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
