4cfdce6dc1106296eb60231b9741be30107dca97
ruan-luqingnian-r <15991875018@163.com>
2021-09-11 20:12:45 +0800

    哈希表

--- practise/src/myNode/HashTableDemo.java
+++ practise/src/myNode/HashTableDemo.java
@@ -54,6 +54,14 @@ class MyNode{
     public void setNext(MyNode next) {
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        return "MyNode{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
 
 /**
@@ -70,8 +78,42 @@ class NodeList{
      * @param newNode
      */
     public void add(MyNode newNode){
-        //创建辅助节点
 
+        if (head == null){
+            head = null;
+            return;
+        }
+
+        //创建辅助节点
+        MyNode temp = head;
+        //找到链表尾部
+        while (true){
+            if (temp.getNext() == null){
+                break;
+            }
+            temp = temp.getNext();
+        }
+        //插入节点
+        temp.setNext(newNode);
+    }
+
+    /**
+     * 遍历链表
+     */
+    public void list(){
+        if (head == null){
+            System.out.println("链表为空");
+            return;
+        }
+        //创建辅助节点
+        MyNode temp = head;
+        while (true){
+            if (temp.getNext() == null){
+                break;
+            }
+            System.out.println(temp.toString());
+            temp = temp.getNext();
+        }
     }
 
 }
