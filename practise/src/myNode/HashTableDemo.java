ac149d0ecb76f28c3cf6f2aeb6df1780c45a6073
ruan-luqingnian-r <15991875018@163.com>
2021-09-11 17:27:17 +0800

    哈希表

--- practise/src/myNode/HashTableDemo.java
+++ practise/src/myNode/HashTableDemo.java
@@ -3,7 +3,22 @@ package myNode;
 /**
  * @Author: ruan
  * Date: 2021/9/11 17:23
- * @Description:
+ * @Description: 哈希表
  */
 public class HashTableDemo {
+
+}
+
+/**
+ * 定义节点
+ */
+class MyNode{
+    /**
+     * 节点ID
+     */
+    private int id;
+    /**
+     * 节点名
+     */
+    private int name;
 }
