2b0c41f4ee23f1ca7decd78cd838824cd140bbb5
ruan-luqingnian-r <15991875018@163.com>
2021-09-11 17:33:21 +0800

    哈希表

--- practise/src/myNode/HashTableDemo.java
+++ practise/src/myNode/HashTableDemo.java
@@ -20,5 +20,58 @@ class MyNode{
     /**
      * 节点名
      */
-    private int name;
+    private String name;
+    /**
+     * 指向下一个节点
+     */
+    private MyNode next;
+
+    public MyNode(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+}
+
+/**
+ * 定义链表以保存节点信息
+ */
+class NodeList{
+    /**
+     * 创建表头节点
+     */
+    private MyNode head;
+
+    /**
+     * 新增节点
+     * @param newNode
+     */
+    public void add(MyNode newNode){
+        //创建辅助节点
+
+    }
+
 }
