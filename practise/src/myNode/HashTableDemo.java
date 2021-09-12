aa7f3026af918aeab5934990d4afb894c2accb53
ruan-luqingnian-r <15991875018@163.com>
2021-09-12 09:11:15 +0800

    哈希表

--- practise/src/myNode/HashTableDemo.java
+++ practise/src/myNode/HashTableDemo.java
@@ -6,6 +6,18 @@ package myNode;
  * @Description: 哈希表
  */
 public class HashTableDemo {
+    public static void main(String[] args) {
+        MyNode node1 = new MyNode(1, "测试1");
+        MyNode node2 = new MyNode(2, "测试2");
+        MyNode node3 = new MyNode(3, "测试3");
+        MyNode node4 = new MyNode(4, "测试4");
+        NodeList nodeList = new NodeList();
+        nodeList.add(node1);
+        nodeList.add(node2);
+        nodeList.add(node3);
+        nodeList.add(node4);
+
+    }
 
 }
 
@@ -80,7 +92,7 @@ class NodeList{
     public void add(MyNode newNode){
 
         if (head == null){
-            head = null;
+            head = newNode;
             return;
         }
 
@@ -100,20 +112,33 @@ class NodeList{
     /**
      * 遍历链表
      */
-    public void list(){
+    public void list(int no){
         if (head == null){
-            System.out.println("链表为空");
+            System.out.println("第"+(no + 1)+"个链表为空");
             return;
+        }else {
+            System.out.print("第"+(no + 1)+"个链表的信息为");
         }
+        
         //创建辅助节点
         MyNode temp = head;
         while (true){
+            System.out.printf(" => id=%d name=%s\t",temp.getId(),temp.getName());
             if (temp.getNext() == null){
                 break;
             }
-            System.out.println(temp.toString());
             temp = temp.getNext();
+            System.out.println(temp.toString());
         }
+        System.out.println();
+    }
+
+    /**
+     * 根据编号查找信息
+     * @param no
+     */
+    public void find(int no){
+
     }
 
 }
