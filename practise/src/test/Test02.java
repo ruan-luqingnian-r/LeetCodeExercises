a93eb99cbbe996db0edc13b9993cfc902795c5c0
ruan-luqingnian-r <15991875018@163.com>
2021-09-13 14:44:43 +0800

    Test02.java

--- practise/src/test/Test02.java
+++ practise/src/test/Test02.java
@@ -11,7 +11,7 @@ public class Test02 {
 
     public static void main(String[] args) {
         int[] arr = {1,2,32,1,32,13123,123,1242,13124,12,3213,4,12,2,3,4,2,2,2,4,5,6,7};
-        bubbleSort(arr);
+        selectSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -34,7 +34,33 @@ public class Test02 {
     }
 
 
+    /**
+     * 选择排序
+     * @param arr
+     * 时间复杂度
+     * O(n^2)
+     */
     public static void selectSort(int[] arr){
-        //
+        //初始默认第一个数值为最小值
+        //遍历后续数值直到找出真正的最小值
+        for (int i = 0; i < arr.length; i++) {
+            //保存下标与最小值
+            int minIndex = i;
+            int minValue = arr[i];
+            //判断后面的值是否有更小的
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < minValue){
+                    //更新保存数据作为新的最小值
+                    minIndex = j;
+                    minValue = arr[j];
+                }
+            }
+            //进行数据交换
+            if (minIndex != i){
+                arr[minIndex] = arr[i];
+                arr[i] = minValue;
+            }
+        }
     }
+
 }
