33f0c36aeeb3f11c61ae4121368c8783ee9048c8
ruan-luqingnian-r <15991875018@163.com>
2021-09-17 14:40:49 +0800

    MyArrayList.java

--- practise/src/tree/BinarySortTreeDemo.java
+++ practise/src/tree/BinarySortTreeDemo.java
@@ -5,6 +5,7 @@ package tree;
  * Date: 2021/9/17 14:31
  * @Description: 二叉排序树
  * 对于二叉排序树的任何一个非叶子节点要求左子节点的值小于当前节点值，右子节点的值大于当前子节点的值
+ * ps：如果有相同值，可以将该节点放在左或者右
  */
-public class BinarySortTree {
+public class BinarySortTreeDemo {
 }
