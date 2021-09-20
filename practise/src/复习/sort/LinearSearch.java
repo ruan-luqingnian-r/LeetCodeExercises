a47e61a9c36fa79bddbdac01f51e4bf7fd73dd32
ruan-luqingnian-r <15991875018@163.com>
2021-09-20 21:04:50 +0800

    LinearSearch.java

---
+++ practise/src/复习/sort/LinearSearch.java
@@ -0,0 +1,25 @@
+package 复习.sort;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: ruan
+ * Date: 2021/9/20 20:57
+ * @Description:
+ */
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {72, 71, 68, 68, 59, 55, 45, 43, 41, 37,32,31,30,30,23,13};
+        ArrayList<Integer> search = search(arr, 30);
+        System.out.println(search);
+    }
+    public static ArrayList<Integer> search(int[] arr, int target){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
