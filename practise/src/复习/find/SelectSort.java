77aad3c81bf62f0aea0c67a966f3320f417e5a56
ruan-luqingnian-r <15991875018@163.com>
2021-09-21 09:42:31 +0800

    二分查找

---
+++ practise/src/复习/find/SelectSort.java
@@ -0,0 +1,23 @@
+package 复习.find;
+
+import java.util.Arrays;
+
+/**
+ * @Author: ruan
+ * Date: 2021/9/21 9:38
+ * @Description: 选择排序
+ */
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = {72, 68, 71, 45, 68, 59, 37, 55, 41, 43};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sort(int[] arr) {
+        for (int i = 0;i < arr.length;i++){
+            //保证下标和变量
+
+        }
+    }
+}
