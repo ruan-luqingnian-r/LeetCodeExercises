d8631b06d831042f32ab34f3aebdbe2dcffc619a
ruan-luqingnian-r <15991875018@163.com>
2021-09-21 10:01:26 +0800

    排序

--- practise/src/复习/sort/InsertionSort.java
+++ practise/src/复习/sort/InsertionSort.java
@@ -1,10 +1,30 @@
 package 复习.sort;
 
+import java.util.Arrays;
+
 /**
  * @Author: ruan
  * Date: 2021/9/21 9:50
  * @Description:
  */
 public class InsertionSort {
-
+    public static void main(String[] args) {
+        int[] arr = {72, 68, 71, 45, 68, 59, 37, 55, 41, 43};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            //待插入数据
+            int value = arr[i];
+            //待插入数据的前一个的下标
+            int index = i - 1;
+            //遍历数组
+            while (index >= 0 && arr[index] > value){
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = value;
+        }
+    }
 }
