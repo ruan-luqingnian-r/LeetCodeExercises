d41b5d5c21e4624bdec7a95b591b975df26e8589
ruan-luqingnian-r <15991875018@163.com>
2021-09-24 11:25:31 +0800

    test

--- practise/src/Test01.java
+++ practise/src/Test01.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -9,5 +10,35 @@ public class Test01 {
     public static void main(String[] args) {
 
 
+
+    }
+}
+class User{
+    private int age;
+    private String name;
+    private Date time;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
     }
 }
