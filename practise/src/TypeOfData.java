3649c27df73872610c6b791680f3b9e2ebfbc8ac
ruan-luqingnian-r <15991875018@163.com>
2021-09-25 17:00:28 +0800

    练习

---
+++ practise/src/TypeOfData.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @Author: ruan
+ * Date: 2021/9/25 16:41
+ * @Description: java数据类型
+ */
+public class TypeOfData {
+
+   /* *//**
+     * 基本数据类型
+     *//*
+    //数值-整数型
+    int anInt;
+    long aLong;
+    byte aByte;
+    short aShort;
+    //数值型-浮点型
+    double aDouble;
+    float aFloat;
+    //字符型
+    char aChar;
+    //布尔型
+    boolean aBoolean;
+    *//**
+     * 引用数据类型
+     *//*
+    String string;
+    Enum anEnum;
+    *//**
+     * 包装类
+     *//*
+    Integer integer;
+    Long aLong;
+    Byte aByte;
+    Short aShort;
+    Double aDouble;
+    Float aFloat;
+    Character character;
+    Boolean aBoolean;
+    *//**
+     * 原子类
+     *//*
+    AtomicInteger atomicInteger;
+    AtomicLong atomicLong;
+    AtomicBoolean aBoolean;
+    .....
+
+    Compare And Swap
+    AbstractQueuedSynchronizer*/
+}
